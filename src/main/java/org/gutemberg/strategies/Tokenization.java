1008e81a5ad1742e80cefd847419c36b4a3c23f8
Jorge Ferrero Linacero <jorge.ferreero.linacero@hp.com>
2019-11-07 09:32:00 +0100

    create tokenization class

---
+++ src/main/java/org/gutemberg/strategies/Tokenization.java
@@ -0,0 +1,18 @@
+package org.gutemberg.strategies;
+
+import org.gutemberg.repository.Repository;
+
+/**
+ * Tokenization
+ */
+public class Tokenization extends Analysis {
+
+    public Tokenization(Repository repository) {
+        super(repository);
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+    }
+}
