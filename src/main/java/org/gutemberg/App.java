f44e2948ae2e3bfee3305939917a06e3f82dd90b
Jorge Ferrero Linacero <jorge.ferreero.linacero@hp.com>
2019-11-08 08:38:15 +0100

    refactor user input

--- src/main/java/org/gutemberg/App.java
+++ src/main/java/org/gutemberg/App.java
@@ -1,9 +1,10 @@
 package org.gutemberg;
 
+import java.util.Scanner;
+
 import org.gutemberg.repository.GutembergRepository;
 import org.gutemberg.strategies.FeaturedWords;
 import org.gutemberg.strategies.Tokenization;
-import org.gutemberg.strategies.UserConsole;
 import org.gutemberg.strategies.WordsCount;
 import org.gutemberg.strategies.NumbersCount;
 
@@ -14,35 +15,43 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
+
+        App.printMenu();
+
+        Scanner sc = new Scanner(System.in);
+        int option = sc.nextInt();
+        sc.close();
+
         GutembergRepository repository = new GutembergRepository("C:\\Git\\gutemberg-analysis\\dataset\\txt");
-
-        UserConsole.printMenu();
-        int option= UserConsole.consoleInt();
-        switch(option){
-            case 1:
-                Tokenization tokenization = new Tokenization(repository);
-                tokenization.execute();
-                break;
-            case 2:
-                FeaturedWords featuredWords = new FeaturedWords(repository);
-                featuredWords.execute();
-                featuredWords.getResult();
-                break;
-            case 3:
-                WordsCount wordsCount = new WordsCount(repository);
-                wordsCount.execute();
-                wordsCount.getResult();
-                break;
-            
-            case 4:    
-                NumbersCount numbersCount = new NumbersCount(repository);
-                numbersCount.execute();
-                numbersCount.getResult();
-                break;
-            case 5:
-
-                break;
-            
+        switch (option) {
+        case 1:
+            Tokenization tokenization = new Tokenization(repository);
+            tokenization.execute();
+            break;
+        case 2:
+            FeaturedWords featuredWords = new FeaturedWords(repository);
+            featuredWords.execute();
+            featuredWords.getResult();
+            break;
+        case 3:
+            WordsCount wordsCount = new WordsCount(repository);
+            wordsCount.execute();
+            wordsCount.getResult();
+            break;
+        case 4:
+            NumbersCount numbersCount = new NumbersCount(repository);
+            numbersCount.execute();
+            numbersCount.getResult();
+            break;
+        case 5:
+            break;
         }
+    }
+
+    private static void printMenu() {
+        System.out.println("Gutenberg Dataset. Enter a number between 1 and 5 for:\n"
+                + "   -1: Tokenize the dataset in different sentences \n" + "   -2: Find the 10 most used words \n"
+                + "   -3: Total number of words \n" + "   -4: Find number of number words \n"
+                + "   -5: Average size of paragraph \n");
     }
 }
